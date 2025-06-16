32f1e403ea57c3bd5970d114d5f7fa5405e3be66
Carlos Eduardo <carlos.eduardo.3832@gmail.com>
2025-06-16 11:28:21 -0300

    Teste oferta destaque

--- src/components/usuario/HomeTab.tsx
+++ src/components/usuario/HomeTab.tsx
@@ -181,13 +181,21 @@ const HomeTab: React.FC<HomeTabProps> = ({
                         <>
                           {/* NOVO: Mostrar imagem da oferta */}
                           {availableOffer.image ? (
-                           <img
-                            src={availableOffer.image}
-                            alt={availableOffer.title}
-                            className="w-full h-20 sm:h-24 md:h-28 object-scale-down rounded-md border border-gray-200 mb-2"
-                          />
+                          //  <img
+                          //   src={availableOffer.image}
+                          //   alt={availableOffer.title}
+                          //   className="w-full h-20 sm:h-24 md:h-28 object-scale-down rounded-md border border-gray-200 mb-2"
+                          // />
+                          <div className="w-full h-20 sm:h-24 md:h-28 flex items-center justify-center bg-gray-50 rounded-md border border-gray-200 mb-2 overflow-hidden">
+                              <img
+                                src={availableOffer.image}
+                                alt={availableOffer.title}
+                                className="max-w-full max-h-full block"
+                                style={{ objectFit: 'scale-down' }}
+                              />
+                            </div>
                           ) : (
-                            <div className="w-full h-24 bg-gray-100 rounded-md border border-gray-200 mb-2 flex items-center justify-center">
+                            <div className="w-full h-20 sm:h-24 md:h-28 bg-gray-100 rounded-md border border-gray-200 mb-2 flex items-center justify-center">
                               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                               </svg>
